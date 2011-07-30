e0943c411a344f5276b486475a4527580d4e63f4
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-07-30 10:41:30 +0000

    DB connection properties is now taken from project.properties

---
+++ branches/autoStation/src/main/java/ua/pogodin/webapp/servlet/Main.java
@@ -0,0 +1,17 @@
+package ua.pogodin.webapp.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("<html>The main servlet is up and running</html>");
+        writer.flush();
+    }
+}
