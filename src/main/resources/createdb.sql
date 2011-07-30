5cc1e2a802294437920e860403af05ffff64203a
sergii.pogodin <sergii.pogodin@gmail.com>
2011-07-30 16:58:23 +0000

    added sql scripts: create and remove of db

---
+++ src/main/resources/createdb.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` text,
+  `password` text,
+  `isdispatcher` tinyint(1) DEFAULT '0',
+  `busid` varchar(45) DEFAULT '0',
+  `name` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+);
+
+CREATE TABLE `busses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `busload` varchar(45) DEFAULT '0',
+  `maxspeed` varchar(45) DEFAULT '0',
+  `workingorder` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+);
+
+CREATE TABLE `bus_application` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `minspeed` varchar(45) DEFAULT '0',
+  `minbusload` varchar(45) DEFAULT '0',
+  `isdone` tinyint(1) DEFAULT '0',
+  `userid` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+);
