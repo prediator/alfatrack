eb19c20f824a7e61cbd2386b7deb7167f1b7b4af
sergii.pogodin <sergii.pogodin@gmail.com>
2011-07-30 21:41:44 +0000

    todo added

--- src/main/java/ua/pogodin/webapp/servlet/Authentication.java
+++ src/main/java/ua/pogodin/webapp/servlet/Authentication.java
@@ -1,21 +1,20 @@
 package ua.pogodin.webapp.servlet;
 
-import java.io.IOException;
+import ua.pogodin.webapp.dao.JdbcConnection;
+import ua.pogodin.webapp.dao.impl.DataBaseConnector;
+import ua.pogodin.webapp.domain.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import ua.pogodin.webapp.dao.JdbcConnection;
-import ua.pogodin.webapp.dao.impl.DataBaseConnector;
-import ua.pogodin.webapp.domain.User;
+import java.io.IOException;
 
 public class Authentication extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		//todo: forward to login jsp
 	}
 
 	@Override
@@ -24,31 +23,25 @@ public class Authentication extends HttpServlet {
 		String pass = req.getParameter("pass");
 		HttpSession currSession = req.getSession(true);
 		JdbcConnection conn = new DataBaseConnector();
-		
-		
-		
-		if(conn.isLoginFree(login)){
+
+		if (conn.isLoginFree(login)){
 			currSession.setAttribute("message", true);
 			resp.sendRedirect("/autoStation");
 		}
 		User user = conn.getUserByLogin(login);
-		
-		
-		
+
 		currSession.setAttribute("user", user);
 		currSession.setAttribute("busApp", null);
-		if(!pass.equals(user.getPassword())){
+		if (!pass.equals(user.getPassword())){
 			currSession.setAttribute("message", true);
 			resp.sendRedirect("/autoStation");
 		}
-		
-		if(user.isDispatcher()){
+
+		if (user.isDispatcher()){
 			getServletContext().getRequestDispatcher("/WEB-INF/jsp/dispatcher.jsp").forward(req, resp);
-		}
-		else{
-			
+		} else{
 			resp.sendRedirect("/autoStation/driver");
 		}
-		
+
 	}
 }
