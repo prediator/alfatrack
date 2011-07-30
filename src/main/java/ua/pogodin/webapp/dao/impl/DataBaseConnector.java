7854e9609de0cc7bbc6f210430cabb8440e629f2
sergii.pogodin <sergii.pogodin@gmail.com>
2011-07-30 15:26:18 +0000

    DAO changed

--- src/main/java/ua/pogodin/webapp/dao/impl/DataBaseConnector.java
+++ src/main/java/ua/pogodin/webapp/dao/impl/DataBaseConnector.java
@@ -7,341 +7,164 @@ package ua.pogodin.webapp.dao.impl;
 import ua.pogodin.webapp.dao.JdbcConnection;
 import ua.pogodin.webapp.domain.User;
 import ua.pogodin.webapp.domain.bus.Bus;
+import ua.pogodin.webapp.util.AppException;
 import ua.pogodin.webapp.util.Properties;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 
  * @author elias
  */
 public class DataBaseConnector implements JdbcConnection {
-	public static final String PROPNAME_DRIVER_CLASS_NAME = "database.driverClassName";
-	public static final String PROPNAME_URL = "database.url";
-	public static final String PROPNAME_USERNAME = "database.username";
-	public static final String PROPNAME_PASSWORD = "database.password";
+    public static final String PROPNAME_DRIVER_CLASS_NAME = "database.driverClassName";
+    public static final String PROPNAME_URL = "database.url";
+    public static final String PROPNAME_USERNAME = "database.username";
+    public static final String PROPNAME_PASSWORD = "database.password";
 
-	private Connection mainConn = null;
-	private PreparedStatement mainPs = null;
+    static {
+        String driverClassName = Properties.get(PROPNAME_DRIVER_CLASS_NAME);
+        try {
+            Class.forName(driverClassName);
+        } catch (ClassNotFoundException e) {
+            throw new Error("jdbc driver" + driverClassName + " not found", e);
+        }
+    }
 
-	public DataBaseConnector() {
+    public boolean isDispatcher(String login) {
+        DbExecutor executor = DbExecutor.execSelect("select isdispatcher from users where login=?", login);
 
-	}
+        try {
+            ResultSet rs = executor.rs();
 
-	private static Connection getDBConnection() {
-		String driverClassName = Properties.get(PROPNAME_DRIVER_CLASS_NAME);
-		try {
-			Class.forName(driverClassName);
-		} catch (ClassNotFoundException e) {
-			throw new Error("jdbc driver" + driverClassName + " not found", e);
-		}
+            if (!rs.next()) {
+                throw new AppException("No user with login " + login);
+            }
 
-		try {
-			return DriverManager.getConnection(Properties.get(PROPNAME_URL),
-					Properties.get(PROPNAME_USERNAME), Properties.get(PROPNAME_PASSWORD));
-		} catch (SQLException e) {
-			throw new Error("can't get connection", e);
-		}
-	}
+            return rs.getBoolean("isdispatcher");
+        } catch (SQLException e) {
+            throw new AppException("Can't find user with login " + login);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-	private void printErrMessage(SQLException e) {
+    public User getUserById(int id) {
+        DbExecutor executor = DbExecutor.execSelect("select * from users where id=?", Integer.toString(id));
+        try {
+            ResultSet rs = executor.rs();
 
-	}
+            if (!rs.next()) {
+                throw new AppException("No user with id " + id);
+            }
 
-	private void setDBUpdate(String sqlRequest) throws SQLException {
-		Connection conn = null;
-		PreparedStatement ps = null;
+            boolean isDispatcher = rs.getBoolean("isdispatcher");
+            return new User(id, rs.getString("login"), rs.getString("password"), rs.getString("name"), isDispatcher,
+                    isDispatcher ? new Bus() : getBusById(rs.getInt("busid")));
+        } catch (SQLException e) {
+            throw new AppException("Can't find user with id " + id);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-		conn = getDBConnection();
-		ps = conn.prepareStatement(sqlRequest);
-		mainConn = conn;
-		mainPs = ps;
-		ps.executeUpdate();
+    public void deleteUserByLogin(String login) {
+        DbExecutor.execUpdate("delete from users where login=?", login);
+    }
 
-		if (ps != null) {
-			ps.close();
-		}
-		if (conn != null) {
-			conn.close();
-		}
-	}
+    public User getUserByLogin(String login) {
+        DbExecutor executor = DbExecutor.execSelect("Select * From users Where login=?", login);
+        try {
+            ResultSet rs = executor.rs();
 
-	private ResultSet getResultSet(String sqlRequest) throws SQLException {
+            if (!rs.next()) {
+                throw new AppException("No user with login " + login);
+            }
 
-		Connection conn = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
+            boolean isDispatcher = rs.getBoolean("isdispatcher");
+            return new User(rs.getInt("id"), login, rs.getString("password"), rs.getString("name"), isDispatcher,
+                    isDispatcher ? new Bus() : getBusById(rs.getInt("busid")));
+        } catch (SQLException e) {
+            throw new AppException("Can't find user with login " + login);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-		try {
+    public Bus getBusById(int id) {
+        DbExecutor executor = DbExecutor.execSelect("select * from busses where id=?", Integer.toString(id));
+        try {
+            ResultSet rs = executor.rs();
 
-			conn = getDBConnection();
-			ps = conn.prepareStatement(sqlRequest);
-			rs = ps.executeQuery();
+            if (!rs.next()) {
+                throw new AppException("No bus with id " + id);
+            }
 
-		} catch (SQLException e) {
-			printErrMessage(e);
-		}
-		return rs;
-	}
+            return new Bus(id, rs.getInt("busload"), rs.getInt("maxspeed"), rs.getBoolean("workingorder"));
+        } catch (SQLException e) {
+            throw new AppException("Can't find bus by id " + id, e);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-	private void closeConnections() throws SQLException {
-		if (mainPs != null) {
-			mainPs.close();
-		}
-		if (mainConn != null) {
-			mainConn.close();
-		}
-	}
+    public void createUser(User user) {
+        int intIsDispatcher = user.isDispatcher() ? 1 : 0;
+        DbExecutor.execUpdate("Insert into users (login,password,isdispatcher,busid,name) values (?,?,?,?,?)",
+                user.getLogin(), user.getPassword(), Integer.toString(intIsDispatcher),
+                Integer.toString(user.getBus().getId()), user.getName());
+    }
 
-	public boolean isDispatcher(String login) {
+    public boolean isLoginFree(String login) {
+        DbExecutor executor = DbExecutor.execSelect("Select count(id) from users where login=?", login);
+        try {
+            if (!executor.rs().next()) {
+                throw new AppException("Can't count users by login");
+            }
+            return executor.rs().getInt(1) == 0;
+        } catch (SQLException e) {
+            throw new AppException("Can't count users by login", e);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-		ResultSet rs = null;
-		String sqlRequest = "SELECT * FROM users WHERE login='" + login + "'";
+    public int getUsersCount() {
+        DbExecutor executor = DbExecutor.execSelect("Select count(id) from users");
+        try {
+            if (!executor.rs().next()) {
+                throw new AppException("Can't extract user count from DB");
+            }
+            return executor.rs().getInt(1);
+        } catch (SQLException e) {
+            throw new AppException("Can't extract user count from DB", e);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 
-		try {
+    public List<User> findAllUsers() {
+        DbExecutor executor = DbExecutor.execSelect("Select * from users");
+        try {
+            List<User> users = new ArrayList<User>();
+            
+            while (executor.rs().next()) {
+                boolean isDispatcher = executor.rs().getBoolean("isdispatcher");
+                int id = executor.rs().getInt("id");
+                String login = executor.rs().getString("login");
+                String password = executor.rs().getString("password");
+                String name = executor.rs().getString("name");
+                Bus bus = isDispatcher ? new Bus() : getBusById(executor.rs().getInt("busid"));
 
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			return (rs.getInt("isdispatcher") == 1);
-
-		} catch (SQLException e) {
-
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-
-		return false;
-	}
-
-	public String getParam(String param, String login) {
-		String sqlRequest = "Select *" + "from users where login='" + login + "'";
-		ResultSet rs = null;
-		String parametr = "";
-
-		try {
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			parametr = rs.getString(param);
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return parametr;
-	}
-
-	public boolean isAccess(String login, String pass) {
-
-		String sqlRequest = "Select * from Users where login='" + login + "'";
-		ResultSet rs = null;
-		String log = "";
-		try {
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			log = rs.getString("login");
-			return log.equals(login);
-
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return false;
-	}
-
-	public User getUserById(int id) {
-
-		User user;
-		ResultSet rs = null;
-		try {
-			String sqlRequest = "Select * From users Where id='" + id + "'";
-			rs = getResultSet(sqlRequest);
-			rs.next();
-
-			boolean isDispatcher = rs.getBoolean("isdispatcher");
-			user = new User(id, rs.getString("login"), rs.getString("password"), rs.getString("name"), isDispatcher,
-					isDispatcher ? new Bus() : getBusById(rs.getInt("busid")));
-
-			return user;
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return null;
-	}
-
-	public void deleteUserByLogin(String login) {
-
-		try {
-			setDBUpdate("Delete From users Where login='" + login + "'");
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-	}
-
-	public User getUserByLogin(String login) {
-
-		User user;
-		ResultSet rs = null;
-		try {
-			String sqlRequest = "Select * From users Where login='" + login + "'";
-			rs = getResultSet(sqlRequest);
-			rs.next();
-
-			boolean isDispatcher = rs.getBoolean("isdispatcher");
-			user = new User(rs.getInt("id"), login, rs.getString("password"), rs.getString("name"), isDispatcher,
-					isDispatcher ? new Bus() : getBusById(rs.getInt("busid")));
-
-			return user;
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return null;
-	}
-
-	public Bus getBusById(int id) {
-
-		Bus bus;
-		ResultSet rs = null;
-		try {
-			String sqlRequest = "Select * From busses Where id='" + id + "'";
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			bus = new Bus(id, rs.getInt("busload"), rs.getInt("maxspeed"), rs.getBoolean("workingorder"));
-			return bus;
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return null;
-	}
-
-	public void createUser(User user) {
-		// if isDispatcher - than 1 - else 0
-		int intIsDispatcher = user.isDispatcher() ? 1 : 0;
-		try {
-			setDBUpdate("Insert into users " + "(login,password,isdispatcher,busid,name) " + "values" + " ('"
-					+ user.getLogin() + "', '" + user.getPassword() + "', " + intIsDispatcher + ","
-					+ user.getBus().getId() + ",'" + user.getName() + "')");
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-	}
-
-	public boolean isLoginFree(String login) {
-
-		ResultSet rs = null;
-		String sqlRequest = "Select count(id) from users where login='" + login + "'";
-		try {
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			// getting first element from request
-			return (rs.getInt(1) == 0);
-
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return false;
-	}
-
-	public int usersCount() {
-
-		ResultSet rs = null;
-		String sqlRequest = "Select count(id) from users";
-		try {
-			rs = getResultSet(sqlRequest);
-			rs.next();
-			// getting first element from request
-			return rs.getInt(1);
-
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-		return 0;
-	}
-
-	public List<User> getAllUsers() {
-
-		List<User> res = new ArrayList<User>();
-
-		ResultSet rs = null;
-		String sqlRequest = "Select * from users";
-		try {
-			rs = getResultSet(sqlRequest);
-			while (rs.next()) {
-				boolean isDispatcher = rs.getBoolean("isdispatcher");
-				res.add(new User(rs.getInt("id"), rs.getString("login"), rs.getString("password"),
-						rs.getString("name"), isDispatcher, isDispatcher ? new Bus() : getBusById(rs.getInt("busid"))));
-			}
-			return res;
-
-		} catch (SQLException e) {
-			printErrMessage(e);
-		} finally {
-			try {
-				closeConnections();
-			} catch (SQLException e) {
-				printErrMessage(e);
-			}
-		}
-
-		return res;
-
-	}
-
+                users.add(new User(id, login, password, name, isDispatcher, bus));
+            }
+            return users;
+        } catch (SQLException e) {
+            throw new AppException("Can't extract users from DB", e);
+        } finally {
+            DbExecutor.close(executor);
+        }
+    }
 }
