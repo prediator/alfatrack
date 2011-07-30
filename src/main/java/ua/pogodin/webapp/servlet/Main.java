cb573cc852aece32eabeef9fb929a1737b5a7ac6
sergii.pogodin <sergii.pogodin@gmail.com>
2011-07-30 12:37:02 +0000

    MVC: Servlet -> Jsp, model transferred via attribute

--- src/main/java/ua/pogodin/webapp/servlet/Main.java
+++ src/main/java/ua/pogodin/webapp/servlet/Main.java
@@ -5,18 +5,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-
-import ua.pogodin.webapp.domain.*;
 
 public class Main extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("<html>The main servlet is up and running</html>");
-        writer.flush();
+        List<User> users = new ArrayList<User>();
+        users.add(new User("Ann"));
+        users.add(new User("Boris"));
+        users.add(new User("Clay"));
+        users.add(new User("Dick"));
+
+        req.setAttribute("users", users);
+        getServletContext().getRequestDispatcher("/WEB-INF/jsp/users.jsp").forward(req, resp);
+    }
+
+    public static class User {
+        private String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
     
     private List<User> getAllUsers(){
