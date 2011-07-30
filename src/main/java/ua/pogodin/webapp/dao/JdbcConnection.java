558628d700a6be695cfdf771e54452eacbc198b5
ipohodin <ipohodin@gmail.com>
2011-07-30 09:03:12 +0000

    addded DAO classes

--- src/main/java/ua/pogodin/webapp/dao/JdbcConnection.java
+++ src/main/java/ua/pogodin/webapp/dao/JdbcConnection.java
@@ -1,5 +1,18 @@
 package ua.pogodin.webapp.dao;
 
+import ua.pogodin.webapp.domain.User;
+import ua.pogodin.webapp.domain.bus.Bus;
+
 public interface JdbcConnection {
+	public boolean isDispatcher(String login);
+	public String getParam(String param, String login);
+	public boolean isAccess(String login, String pass) ;
+	public User getUserById(int id);
+	public void deleteUserByLogin(String login);
+	public User getUserByLogin(String login) ;
+	public Bus getBusById(int id) ;
+	public void createUser(User user) ;
+	public boolean isLoginFree(String login);
+	public int usersCount() ;
 
 }
