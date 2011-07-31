7b05212f9128a26d1beb294f627e866112522dda
ipohodin <ipohodin@gmail.com>
2011-07-31 10:58:19 +0000

    change driver

--- src/main/java/ua/pogodin/webapp/servlet/Driver.java
+++ src/main/java/ua/pogodin/webapp/servlet/Driver.java
@@ -17,17 +17,38 @@ import ua.pogodin.webapp.domain.User;
 import com.mysql.jdbc.interceptors.SessionAssociationInterceptor;
 
 public class Driver extends HttpServlet {
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-	}
-
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession currSession = req.getSession();
 		User user = (User) currSession.getAttribute("user");
+		boolean isBusWorking = Integer.valueOf((String)req.getAttribute("workingOrder")).intValue() == 1;
+		user.getBus().setWorkingOrder(isBusWorking);
 		JdbcConnection conn = new DataBaseConnector();
 		List<BusApplication> apps = conn.findAllBusApplications();
+		resultRequest(user,apps,req,resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		HttpSession currSession = req.getSession(false);
+		if (currSession == null){
+			resp.sendRedirect("/autoStation");
+		}
+		User user = (User)currSession.getAttribute("user");
+		
+		
+		JdbcConnection conn = new DataBaseConnector();
+		List<BusApplication> apps = conn.findAllBusApplications();
+		resultRequest(user,apps,req,resp);
+		
+	}
+
+	
+
+	private void resultRequest(User user, List<BusApplication> apps, HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException{ 
 		req.setAttribute("apps", apps);
 		req.setAttribute("user", user);
 		getServletContext().getRequestDispatcher("/WEB-INF/jsp/driver.jsp").forward(req, resp);
