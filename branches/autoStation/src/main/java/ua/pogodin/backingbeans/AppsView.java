383b24191dc6fb4acca1f58526ebe7ae41a2b547
ipohodin@gmail.com <ipohodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-09-05 15:49:36 +0000

    change view for apps

--- branches/autoStation/src/main/java/ua/pogodin/backingbeans/AppsView.java
+++ branches/autoStation/src/main/java/ua/pogodin/backingbeans/AppsView.java
@@ -1,34 +1,52 @@
 package ua.pogodin.backingbeans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.http.HttpSession;
 
 import ua.pogodin.webapp.dao.DbConnection;
 import ua.pogodin.webapp.domain.BusApplication;
+import ua.pogodin.webapp.domain.Driver;
+import ua.pogodin.webapp.domain.User;
+import ua.pogodin.webapp.util.AppsUtil;
 
 @ManagedBean(name = "apps")
 @ApplicationScoped
 public class AppsView {
 
-	private List<BusApplication> apps;
+	private List<AppsUtil> apps;
+	private User user;
 
 	public AppsView() throws NamingException {
-
 		DbConnection dbConn = (DbConnection) new InitialContext()
-				.lookup("autostation");
-		apps = dbConn.findAllBusApplications();
-
+				.lookup("java:comp/env/AccountManagmentLocal");
+		apps = new ArrayList<AppsUtil>();
+		
+		List<Driver> allDrivers = dbConn.getAllDrivers();
+		for (BusApplication app : dbConn.findAllBusApplications()) {
+			apps.add(new AppsUtil(app));
+			apps.get(apps.size() - 1).setDriversOn(dbConn.getDriversByAppId(app.getId()),allDrivers);
+		}
+		HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+		user = (User)session.getAttribute("user");
 	}
 
-	public List<BusApplication> getApps() {
+	public List<AppsUtil> getApps() {
 		return apps;
 	}
 
 	public String getHello() {
 		return "hello";
 	}
+
+	public User getUser() {
+		return user;
+	}
+	
 }
