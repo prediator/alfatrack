bb8c690a8da79f1de79e16fe9bb2a09b194adf7d
ipohodin@gmail.com <ipohodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-09-05 12:31:57 +0000

    add backingBeans step3 + views(of pages) + change web.xml + add faces-config.xml

--- branches/autoStation/src/main/java/ua/pogodin/backingbeans/AppsView.java
+++ branches/autoStation/src/main/java/ua/pogodin/backingbeans/AppsView.java
@@ -1,5 +1,34 @@
 package ua.pogodin.backingbeans;
 
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ua.pogodin.webapp.dao.DbConnection;
+import ua.pogodin.webapp.domain.BusApplication;
+
+@ManagedBean(name = "apps")
+@ApplicationScoped
 public class AppsView {
 
+	private List<BusApplication> apps;
+
+	public AppsView() throws NamingException {
+
+		DbConnection dbConn = (DbConnection) new InitialContext()
+				.lookup("autostation");
+		apps = dbConn.findAllBusApplications();
+
+	}
+
+	public List<BusApplication> getApps() {
+		return apps;
+	}
+
+	public String getHello() {
+		return "hello";
+	}
 }
