f4458e9c822b5e3ad9cfeab64f71c5cc832df4a8
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-26 23:01:05 +0000

    StraightFlush Definer implemented

--- branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/StraightFlushDefiner.java
+++ branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/StraightFlushDefiner.java
@@ -1,12 +1,51 @@
 package ua.pogodin.poker.pockerhand.definer;
 
+import ua.pogodin.poker.card.Card;
+import ua.pogodin.poker.card.Rank;
+import ua.pogodin.poker.card.Suit;
 import ua.pogodin.poker.cards.Hand;
+
+import java.util.List;
 
 /**
  * @author Sergii Pogodin
  */
+@SuppressWarnings("JavaDoc")
 class StraightFlushDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return false;  //todo pogodin
+        List<Card> cards = hand.getCards();
+        return isTheSameSuit(cards) && isRankInARow(cards);
     }
+
+    private boolean isTheSameSuit(List<Card> cards) {
+        Suit suit = cards.get(0).getSuit();
+        for (int i = 1; i < cards.size(); i++) {
+            if (cards.get(i).getSuit() != suit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * return true if A5432, AKQJT or similar
+     */
+    private boolean isRankInARow(List<Card> cards) {
+        return isRankTop4HigherThenRankBottom(cards) || isTopAceAndFive(cards);
+    }
+
+    /**
+     * return true if AKQJT or similar
+     */
+    private boolean isRankTop4HigherThenRankBottom(List<Card> cards) {
+        return cards.get(4).getRank().ordinal() - cards.get(0).getRank().ordinal() == 4;
+    }
+
+    /**
+     * return true if A5432
+     */
+    private boolean isTopAceAndFive(List<Card> cards) {
+        return cards.get(0).getRank() == Rank.Ace && cards.get(1).getRank() == Rank.Five;
+    }
+
 }
