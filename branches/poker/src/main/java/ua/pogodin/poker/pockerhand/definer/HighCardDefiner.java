6bfed695b2fd1f3cab91c9d1fe57210eae4c1796
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-26 22:16:46 +0000

    HighCardDefiner implemented

--- branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/HighCardDefiner.java
+++ branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/HighCardDefiner.java
@@ -5,6 +5,6 @@ package ua.pogodin.poker.pockerhand.definer;
  */
 public class HighCardDefiner implements PokerHandDefiner {
     public boolean define() {
-        return false;  //todo pogodin
+        return true;
     }
 }
