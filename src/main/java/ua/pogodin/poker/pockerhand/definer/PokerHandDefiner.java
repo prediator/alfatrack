f6ef3d0ff787177b5cd0a4271f20f3e905285cc2
sergii.pogodin <sergii.pogodin@gmail.com>
2011-10-26 22:36:43 +0000

    fixed Definer's API: now it's parameter is Hand

--- src/main/java/ua/pogodin/poker/pockerhand/definer/PokerHandDefiner.java
+++ src/main/java/ua/pogodin/poker/pockerhand/definer/PokerHandDefiner.java
@@ -1,8 +1,10 @@
 package ua.pogodin.poker.pockerhand.definer;
 
+import ua.pogodin.poker.cards.Hand;
+
 /**
  * @author Sergii Pogodin
  */
 public interface PokerHandDefiner {
-    boolean define();
+    boolean define(Hand hand);
 }
--- src/main/java/ua/pogodin/poker/pockerhand/definer/TwoPairDefiner.java
+++ src/main/java/ua/pogodin/poker/pockerhand/definer/TwoPairDefiner.java
@@ -1,10 +1,12 @@
 package ua.pogodin.poker.pockerhand.definer;
 
+import ua.pogodin.poker.cards.Hand;
+
 /**
  * @author Sergii Pogodin
  */
 class TwoPairDefiner implements PokerHandDefiner {
-    public boolean define() {
+    public boolean define(Hand hand) {
         return false;  //todo pogodin
     }
 }
