16ea576a04afbd01418dc1ca041c968cb527b941
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-27 23:44:37 +0000

    HandAndDeck.toString()

--- branches/poker/src/test/java/ua/pogodin/poker/cards/HandAndDeckTest.java
+++ branches/poker/src/test/java/ua/pogodin/poker/cards/HandAndDeckTest.java
@@ -2,6 +2,13 @@ package ua.pogodin.poker.cards;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ua.pogodin.poker.card.Card;
+import ua.pogodin.poker.card.Rank;
+import ua.pogodin.poker.card.Suit;
+
+import java.util.Arrays;
+
+import static org.testng.Assert.assertEquals;
 
 /**
  * @author Sergii Pogodin
@@ -15,7 +22,7 @@ public class HandAndDeckTest {
 
     @DataProvider
     public Object[][] invalid() {
-        return new Object[][] {
+        return new Object[][]{
                 {"2H 2S 3H 3S 3C 2D 3D 6C 9C TH "},
                 {"2H2S 3H 3S 3C 2D 3D 6C 9C TH "},
                 {"6CF9C 8C 2D 7C 2H TC 4C 9S AH"},
@@ -26,5 +33,25 @@ public class HandAndDeckTest {
     @Test(dataProvider = "invalid", expectedExceptions = IllegalArgumentException.class)
     public void shouldThrowExceptionOnIncorrectIncomeString(String incorrectAbbreviation) throws Exception {
         HandAndDeck.parse(incorrectAbbreviation);
+    }
+
+    @Test
+    public void toStringShouldReturnAbbreviations() throws Exception {
+        HandAndDeck handAndDeck = new HandAndDeck(
+                new Hand(Arrays.asList(
+                        new Card(Rank.King, Suit.Clubs),
+                        new Card(Rank.Four, Suit.Hearts),
+                        new Card(Rank.Eight, Suit.Spades),
+                        new Card(Rank.Seven, Suit.Clubs),
+                        new Card(Rank.Ace, Suit.Diamonds)
+                )),
+                new Deck(Arrays.asList(
+                        new Card(Rank.Ten, Suit.Spades),
+                        new Card(Rank.Ace, Suit.Diamonds),
+                        new Card(Rank.Nine, Suit.Hearts),
+                        new Card(Rank.Five, Suit.Diamonds),
+                        new Card(Rank.Queen, Suit.Clubs)
+                )));
+        assertEquals(handAndDeck.toString(), "Hand: AD KC 8S 7C 4H Deck: TS AD 9H 5D QC");
     }
 }
