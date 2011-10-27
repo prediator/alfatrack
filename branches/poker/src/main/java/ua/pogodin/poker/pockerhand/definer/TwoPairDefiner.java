b9ed8853ef9b7371189d41a7a83fdd38744bf9cc
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-27 22:29:53 +0000

    TwoPair Definer implemented

--- branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/TwoPairDefiner.java
+++ branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/TwoPairDefiner.java
@@ -2,11 +2,13 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.areQuantitiesOfSameKind;
+
 /**
  * @author Sergii Pogodin
  */
 class TwoPairDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return false;  //todo pogodin
+        return areQuantitiesOfSameKind(hand.getCards(), 2, 2, 1);
     }
 }
