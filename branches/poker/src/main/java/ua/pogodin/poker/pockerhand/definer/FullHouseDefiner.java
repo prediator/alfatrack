c84eb4515bd494042b08b623b93c659fa7397476
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-27 21:58:12 +0000

    FullHouse Definer implemented

--- branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/FullHouseDefiner.java
+++ branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/FullHouseDefiner.java
@@ -2,11 +2,13 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.areQuantitiesOfSameKind;
+
 /**
  * @author Sergii Pogodin
  */
 class FullHouseDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return false;  //todo pogodin
+        return areQuantitiesOfSameKind(hand, 3, 2);
     }
 }
---
+++ branches/poker/src/test/java/ua/pogodin/poker/pockerhand/definer/FullHouseDefinerTest.java
@@ -0,0 +1,21 @@
+package ua.pogodin.poker.pockerhand.definer;
+
+/**
+ * @author Sergii Pogodin
+ */
+public class FullHouseDefinerTest extends AbstractDefinerTest {
+    @Override
+    protected Object[][] getDataProvider() {
+        return new Object[][]{
+                {"AS 4D AC AD 4H", true},
+                {"5S QH QS 5D QC", true},
+                {"3C 2C 2D 2H 2S", false},
+                {"AS 4D AC KD 4H", false},
+        };
+    }
+
+    @Override
+    protected PokerHandDefiner getDefiner() {
+        return new FullHouseDefiner();
+    }
+}
