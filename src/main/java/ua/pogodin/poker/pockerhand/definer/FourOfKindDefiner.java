10b67d013251c4d300469c01bd01ebaa33c847f3
sergii.pogodin <sergii.pogodin@gmail.com>
2011-10-27 21:50:49 +0000

    FourOfKind Definer implemented

--- src/main/java/ua/pogodin/poker/pockerhand/definer/FourOfKindDefiner.java
+++ src/main/java/ua/pogodin/poker/pockerhand/definer/FourOfKindDefiner.java
@@ -2,11 +2,17 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import java.util.Arrays;
+
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.calcSameRankCardsQuantities;
+
 /**
  * @author Sergii Pogodin
  */
 class FourOfKindDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return false;  //todo pogodin
+        Integer[] quantities = calcSameRankCardsQuantities(hand.getCards());
+        return Arrays.equals(quantities, new Integer[]{4, 1});
     }
+
 }
