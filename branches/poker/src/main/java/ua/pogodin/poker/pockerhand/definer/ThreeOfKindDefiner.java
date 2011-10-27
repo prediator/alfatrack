20619d2675f5729567715ca1bda67a52f6063179
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-27 22:29:00 +0000

    static import for DefinerUtils methods invocations

--- branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/ThreeOfKindDefiner.java
+++ branches/poker/src/main/java/ua/pogodin/poker/pockerhand/definer/ThreeOfKindDefiner.java
@@ -2,11 +2,13 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.areQuantitiesOfSameKind;
+
 /**
  * @author Sergii Pogodin
  */
 class ThreeOfKindDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return DefinerUtils.areQuantitiesOfSameKind(hand.getCards(), 3, 1, 1);
+        return areQuantitiesOfSameKind(hand.getCards(), 3, 1, 1);
     }
 }
