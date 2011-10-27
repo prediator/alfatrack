4647443ec1b7cf8df552f643dbb3535cd6f219a6
sergii.pogodin@gmail.com <sergii.pogodin@gmail.com@a6fd9b19-06c6-9c1e-42fe-9991268223a5>
2011-10-27 21:50:49 +0000

    FourOfKind Definer implemented

---
+++ branches/poker/src/test/java/ua/pogodin/poker/pockerhand/definer/FourOfKindDefinerTest.java
@@ -0,0 +1,21 @@
+package ua.pogodin.poker.pockerhand.definer;
+
+/**
+ * @author Sergii Pogodin
+ */
+public class FourOfKindDefinerTest extends AbstractDefinerTest {
+    @Override
+    protected Object[][] getDataProvider() {
+        return new Object[][]{
+                {"AS 4D AC AD AH", true},
+                {"5S 5H QH 5D 5C", true},
+                {"3C 2C 2D 2H 2S", true},
+                {"AS 4D AC AD 4H", false},
+        };
+    }
+
+    @Override
+    protected PokerHandDefiner getDefiner() {
+        return new FourOfKindDefiner();
+    }
+}
