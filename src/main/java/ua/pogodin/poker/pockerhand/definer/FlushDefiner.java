f90e2766b692f2a51acdb9fec9220d9d461248cf
sergii.pogodin <sergii.pogodin@gmail.com>
2011-10-27 22:29:00 +0000

    static import for DefinerUtils methods invocations

--- src/main/java/ua/pogodin/poker/pockerhand/definer/FlushDefiner.java
+++ src/main/java/ua/pogodin/poker/pockerhand/definer/FlushDefiner.java
@@ -2,11 +2,13 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.isTheSameSuit;
+
 /**
  * @author Sergii Pogodin
  */
 class FlushDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return DefinerUtils.isTheSameSuit(hand.getCards());
+        return isTheSameSuit(hand.getCards());
     }
 }
--- src/main/java/ua/pogodin/poker/pockerhand/definer/StraightDefiner.java
+++ src/main/java/ua/pogodin/poker/pockerhand/definer/StraightDefiner.java
@@ -2,11 +2,13 @@ package ua.pogodin.poker.pockerhand.definer;
 
 import ua.pogodin.poker.cards.Hand;
 
+import static ua.pogodin.poker.pockerhand.definer.DefinerUtils.isRankInARow;
+
 /**
  * @author Sergii Pogodin
  */
 class StraightDefiner implements PokerHandDefiner {
     public boolean define(Hand hand) {
-        return DefinerUtils.isRankInARow(hand.getCards());
+        return isRankInARow(hand.getCards());
     }
 }
